1c0f59adfa75a6362695765253e3fdab485f4082
suman chapai <sumanchapai@gmail.com>
2023-09-30 09:59:36 -0500

    Add title, heading

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 const withNextra = require('nextra')({
+  title: 'Physics 150, Luther College',
+  description: 'Engineering 101 course at Luther',
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
