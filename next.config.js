06b5067c44f529d27ecc660aa0e6fe24322f2556
suman chapai <sumanchapai@gmail.com>
2023-09-30 10:30:13 -0500

    Update next head

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 const withNextra = require('nextra')({
-  title: 'Physics 150, Luther College',
-  description: 'Engineering 101 course at Luther',
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+
+
 
 const config: DocsThemeConfig = {
   logo: <span>PHY-150</span>,
@@ -9,6 +13,55 @@ const config: DocsThemeConfig = {
   docsRepositoryBase: 'https://github.com/sumanchapai/phy-150-rc',
   footer: {
     text: 'Luther College | Engineering - 2023FA',
+  },
+  head: function useHead() {
+    const { title } = useConfig()
+    const { route } = useRouter()
+    const socialCard =
+      route === '/' || !title
+        ? 'https://nextra.site/og.jpeg'
+        : `https://nextra.site/api/og?title=${title}`
+
+    return (
+      <>
+        <meta name="msapplication-TileColor" content="#fff" />
+        <meta name="theme-color" content="#fff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta
+          name="description"
+          content="Engineering 101 course at Luther"
+        />
+        <meta
+          name="og:description"
+          content="Engineering 101 course at Luther"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={socialCard} />
+        <meta name="twitter:site:domain" content="nextra.site" />
+        <meta name="twitter:url" content="https://nextra.site" />
+        <meta
+          name="og:title"
+          content={title ? title + ' – Physics 150 Luther' : 'Physics 150 Luther College'}
+        />
+        <meta name="og:image" content={socialCard} />
+        <meta name="apple-mobile-web-app-title" content="Nextra" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link
+          rel="icon"
+          href="/favicon-dark.svg"
+          type="image/svg+xml"
+          media="(prefers-color-scheme: dark)"
+        />
+        <link
+          rel="icon"
+          href="/favicon-dark.png"
+          type="image/png"
+          media="(prefers-color-scheme: dark)"
+        />
+      </>
+    )
   },
 }
 
